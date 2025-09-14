488b2ff32d7c72dce7d9ef0a7fbf61e07f0d00fb
Andy Smith <smith780@gmail.com>
2025-09-14 14:42:01 -0400

    Update symbols

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,7 @@
 const https = require('https')
 const weatherConfig = require('./weatherConfig')
 const apiUrls = require('./apiUrls')
+const getWeatherSymbol = require('./getWeatherSymbol')
 
 module.exports = {
     getDailyForcast() {
@@ -34,18 +35,23 @@ module.exports = {
         let currentTemp = data.main.temp
         let maxTemp = data.main.temp_max
         let minTemp = data.main.temp_min
+        let desId = data.weather[0].id
         let description = data.weather[0].description
         let mainDes = data.weather[0].main
         let sunrise = new Date(data.sys.sunrise*1000)
         let sunset = new Date(data.sys.sunset*1000)
         let location = data.name
         let timezone = data.timezone
+        let symbol = getWeatherSymbol.getSymbol(desId)
 
-        console.log(`   Today's Forcast for ${location}\n`)        
+        console.log(`   Today's Forcast for ${location}\n`) 
+        console.log(symbol)
         console.log(`
         Current Temp: ${Math.round(currentTemp)}\n
         High: ${Math.round(maxTemp)}\n
         Low: ${Math.round(minTemp)}\n
+        Des Id: ${desId}\n
+        Main: ${mainDes}\n
         Description: ${description}\n
         Sunrise: ${sunrise.toLocaleTimeString()}\n
         Sunset: ${sunset.toLocaleTimeString()}\n`)
